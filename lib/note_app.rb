57072cb82e6e49682400e1502be9502825acf64b
Lovelyn <lovelynisrael@hotmail.com>
2016-02-21 11:03:07 +0100

    Create GET and SEARCH methods

--- lib/note_app.rb
+++ lib/note_app.rb
@@ -13,6 +13,28 @@ module Note
             @notes.each do |i|
                puts "NOTE ID : #{@notes.index(i)+1}\n\n#{i}\n\nBy Author: #{@author}\n\n===============================\n\n"
            end
+        end 
+        def get(note_id)
+            if note_id < 1 || note_id > @notes.length
+                puts "Invalid ID: #{note_id}\n\n"
+            else
+                puts @notes[note_id - 1]
+            end
+        end
+        
+        def search(search_text)
+            puts "Showing results for search '#{search_text}':\n\n"
+            @notes.each do |i|
+                if i.include? search_text
+                    puts "NOTE ID : #{@notes.index(i)+1}\n\n#{i}\n\nBy Author: #{@author}\n\n===============================\n\n"
+                
+                else
+                    puts "No result found"
+                    break
+                end
+                
+            end
+            
         end
     end
 end

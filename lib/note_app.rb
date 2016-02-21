aed6f8b19e5a3dae173406c0d3ae62217ba22908
Lovelyn <lovelynisrael@hotmail.com>
2016-02-21 11:01:33 +0100

    Create CREATE and LIST methods

--- lib/note_app.rb
+++ lib/note_app.rb
@@ -5,5 +5,14 @@ module Note
             @author = author
             @notes = []
         end
+        def create(notes_content)
+            @notes.push(notes_content)
+        end
+        
+        def list
+            @notes.each do |i|
+               puts "NOTE ID : #{@notes.index(i)+1}\n\n#{i}\n\nBy Author: #{@author}\n\n===============================\n\n"
+           end
+        end
     end
 end
